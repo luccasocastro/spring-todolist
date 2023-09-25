296a836b316952c380cbad776509266579976db3
Luccas Souza <emaildocastro.adm01@gmail.com>
2023-09-24 23:10:07 -0300

    test: creating sql for test database

---
+++ src/test/resources/import.sql
@@ -0,0 +1,12 @@
+DELETE FROM tb_todos;
+
+INSERT INTO tb_todos (id, name, description, realized, priority)
+ VALUES (9995, '@luccasocastro', 'bla bla bla', false, 1);
+INSERT INTO tb_todos (id, name, description, realized, priority)
+ VALUES (9996, '@luccasocastro', 'bla bla bla', false, 1);
+INSERT INTO tb_todos (id, name, description, realized, priority)
+ VALUES (9997, '@luccasocastro', 'bla bla bla', false, 1);
+INSERT INTO tb_todos (id, name, description, realized, priority)
+ VALUES (9998, '@luccasocastro', 'bla bla bla', false, 1);
+INSERT INTO tb_todos (id, name, description, realized, priority)
+ VALUES (9999, '@luccasocastro', 'bla bla bla', false, 1);
---
+++ src/test/resources/remove.sql
@@ -0,0 +1,1 @@
+TRUNCATE TABLE tb_todos;
