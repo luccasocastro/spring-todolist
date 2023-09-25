09d242b411c5ec66af637b9fa73c2a4e30443f9a
Luccas Souza <emaildocastro.adm01@gmail.com>
2023-09-24 22:25:38 -0300

    test: adding tests 1

--- src/test/java/com/luxkapotter/springtodolist/SpringTodolistApplicationTests.java
+++ src/test/java/com/luxkapotter/springtodolist/SpringTodolistApplicationTests.java
@@ -1,13 +1,39 @@
 package com.luxkapotter.springtodolist;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import com.luxkapotter.springtodolist.entity.Todo;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class SpringTodolistApplicationTests {
+	@Autowired
+	private WebTestClient webTestClient;
 
 	@Test
-	void contextLoads() {
+	void testCreateTodoSuccess() {
+		var todo = new Todo("todo 1", "todo 1 desc", false, 1);
+
+		webTestClient
+			.post()
+			.uri("/todos")
+			.bodyValue(todo)
+			.exchange()
+			.expectStatus().isCreated()
+			.expectBody()
+			.jsonPath("$").isArray()
+			.jsonPath("$.length()").isEqualTo(1)
+			.jsonPath("$[0].name").isEqualTo(todo.getName())
+			.jsonPath("$[0].description").isEqualTo(todo.getDescription())
+			.jsonPath("$[0].realized").isEqualTo(todo.isRealized())
+			.jsonPath("$[0].priority").isEqualTo(todo.getPriority());
+	}
+
+	@Test
+	void testCreateTodoFailure() {
 	}
 
 }
