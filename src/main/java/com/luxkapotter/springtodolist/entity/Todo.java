2839c6b20d2d13014fe9b629ae9b0d37570fbb88
Luccas Souza <emaildocastro.adm01@gmail.com>
2023-09-24 21:45:09 -0300

    feat: adding annotations

--- src/main/java/com/luxkapotter/springtodolist/entity/Todo.java
+++ src/main/java/com/luxkapotter/springtodolist/entity/Todo.java
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 @Table(name="tb_todos")
@@ -13,12 +14,22 @@ public class Todo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank
     private String name;
+    @NotBlank
     private String description;
     private boolean realized;
     private int priority;
     
     public Todo(String name, String description, boolean realized, int priority) {
+        this.name = name;
+        this.description = description;
+        this.realized = realized;
+        this.priority = priority;
+    }
+
+    public Todo(Long id, @NotBlank String name, @NotBlank String description, boolean realized, int priority) {
+        this.id = id;
         this.name = name;
         this.description = description;
         this.realized = realized;
@@ -66,6 +77,31 @@ public class Todo {
 
     public void setPriority(int priority) {
         this.priority = priority;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Todo other = (Todo) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
     }
 
     
